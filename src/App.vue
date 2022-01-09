1358697e991ea1d9cbe59567b6b86e1ea95d9ac2
SerhiiCho <serhiicho@protonmail.com>
2022-01-09 21:44:04 +0200

    Added counter example

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 import OptionsApiModal from './components/OptionsApiModal.vue'
 import CompositionApiModal from './components/CompositionApiModal.vue'
 import ScriptSetupModal from './components/ScriptSetupModal.vue'
+import Counter from './components/Counter.vue'
 </script>
 
 <template>
     <OptionsApiModal />
     <CompositionApiModal />
     <ScriptSetupModal />
+
+    <div class="border p-5 m-4 rounded">
+        <h1 class="text-xl">Click on the number</h1>
+        <Counter />
+    </div>
 </template>
