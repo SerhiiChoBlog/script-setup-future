03851c0aca0fc2946a54fd662ebc9cd0538f85df
SerhiiCho <serhiicho@protonmail.com>
2022-01-09 22:26:08 +0200

    Changed spinner example

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,25 @@ import OptionsApiModal from './components/OptionsApiModal.vue'
 import CompositionApiModal from './components/CompositionApiModal.vue'
 import ScriptSetupModal from './components/ScriptSetupModal.vue'
 import Counter from './components/Counter.vue'
+import Spinner from './components/Spinner.vue'
 </script>
 
 <template>
-    <OptionsApiModal />
-    <CompositionApiModal />
-    <ScriptSetupModal />
+    <div class="border p-5 m-4 rounded">
+        <h1 class="text-xl">Modals examples</h1>
+
+        <OptionsApiModal />
+        <CompositionApiModal />
+        <ScriptSetupModal />
+    </div>
 
     <div class="border p-5 m-4 rounded">
         <h1 class="text-xl">Click on the number</h1>
         <Counter />
+    </div>
+
+    <div class="border p-5 m-4 rounded">
+        <h1 class="text-xl mb-4">Spinner example</h1>
+        <Spinner />
     </div>
 </template>
--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -9,16 +9,17 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <div class="text-center" :class="{ 'center-the-spinner': center }">
-        <div class="spinner-border text-secondary" role="status"></div>
-    </div>
+    <div
+        style="border-top-color:transparent"
+        class="w-10 h-10 border-4 border-blue-400 border-solid rounded-full animate-spin"
+        :class="{ 'center-the-spinner': center }"
+    ></div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
 .center-the-spinner {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
 }
 </style>
