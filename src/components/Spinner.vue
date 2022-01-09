a7d1a72227dc84809a23a4c5eab8aa28d443d192
SerhiiCho <serhiicho@protonmail.com>
2022-01-09 22:18:57 +0200

    Added spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+    center?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+    center: false
+})
+</script>
+
+<template>
+    <div class="text-center" :class="{ 'center-the-spinner': center }">
+        <div class="spinner-border text-secondary" role="status"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.center-the-spinner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
